0271e9cf57337d1ae04f098853a391d6ec65ae00
Landry Kapela <landrykapela@gmail.com>
2019-04-02 13:27:29 +0300

    Update precache-manifest.3b19db06d95ea4004b19685a57908053.js
    Remove root folder from paths

--- precache-manifest.3b19db06d95ea4004b19685a57908053.js
+++ precache-manifest.3b19db06d95ea4004b19685a57908053.js
@@ -1,50 +1,50 @@
 self.__precacheManifest = [
   {
     "revision": "a4e7df3117471ff256348417e430acc6",
-    "url": "/portfolio/static/media/ubj.a4e7df31.png"
+    "url": "static/media/ubj.a4e7df31.png"
   },
   {
     "revision": "beb78b9086f96c85b254",
-    "url": "/portfolio/static/css/main.6cb602ea.chunk.css"
+    "url": "static/css/main.6cb602ea.chunk.css"
   },
   {
     "revision": "53ebc025fca807d4dbea",
-    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+    "url": "static/js/runtime~main.f308c8e6.js"
   },
   {
     "revision": "e185fd3e83ce9a1b599f",
-    "url": "/portfolio/static/js/2.2cf33cb7.chunk.js"
+    "url": "static/js/2.2cf33cb7.chunk.js"
   },
   {
     "revision": "56583bdb03823d5e3cd8a78e1e64e33d",
-    "url": "/portfolio/static/media/fk_logo.56583bdb.png"
+    "url": "static/media/fk_logo.56583bdb.png"
   },
   {
     "revision": "f6e1c259a3e9eb4d487d41c8e0371920",
-    "url": "/portfolio/static/media/abc.f6e1c259.jpg"
+    "url": "static/media/abc.f6e1c259.jpg"
   },
   {
     "revision": "beb78b9086f96c85b254",
-    "url": "/portfolio/static/js/main.c426aac3.chunk.js"
+    "url": "static/js/main.c426aac3.chunk.js"
   },
   {
     "revision": "11f082c524c8a5a318fceb8c4111f1c1",
-    "url": "/portfolio/static/media/jamirex.11f082c5.jpg"
+    "url": "static/media/jamirex.11f082c5.jpg"
   },
   {
     "revision": "8901800ce9ab78fd768e3520f19da754",
-    "url": "/portfolio/static/media/itm.8901800c.png"
+    "url": "static/media/itm.8901800c.png"
   },
   {
     "revision": "48187bdc53744cfd394b42874daf50e3",
-    "url": "/portfolio/static/media/kakobrands.48187bdc.png"
+    "url": "static/media/kakobrands.48187bdc.png"
   },
   {
     "revision": "5e9cdd950d53539a03d228892fc8875a",
-    "url": "/portfolio/static/media/building.5e9cdd95.jpg"
+    "url": "static/media/building.5e9cdd95.jpg"
   },
   {
     "revision": "1ed83618dd5161c230d5177acff97ce1",
-    "url": "/portfolio/index.html"
+    "url": "index.html"
   }
 ];
