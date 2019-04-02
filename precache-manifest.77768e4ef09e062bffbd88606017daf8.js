61b2894c443c1dca07a2253847daa6eb6f82c939
landrykapela <=>
2019-04-02 12:51:07 +0300

    Updates

---
+++ precache-manifest.77768e4ef09e062bffbd88606017daf8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4e7df3117471ff256348417e430acc6",
+    "url": "/static/media/ubj.a4e7df31.png"
+  },
+  {
+    "revision": "00e0b5406d3b64767ceb",
+    "url": "/static/css/main.9e3064ce.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e185fd3e83ce9a1b599f",
+    "url": "/static/js/2.2cf33cb7.chunk.js"
+  },
+  {
+    "revision": "56583bdb03823d5e3cd8a78e1e64e33d",
+    "url": "/static/media/fk_logo.56583bdb.png"
+  },
+  {
+    "revision": "f6e1c259a3e9eb4d487d41c8e0371920",
+    "url": "/static/media/abc.f6e1c259.jpg"
+  },
+  {
+    "revision": "00e0b5406d3b64767ceb",
+    "url": "/static/js/main.51c64e4b.chunk.js"
+  },
+  {
+    "revision": "11f082c524c8a5a318fceb8c4111f1c1",
+    "url": "/static/media/jamirex.11f082c5.jpg"
+  },
+  {
+    "revision": "8901800ce9ab78fd768e3520f19da754",
+    "url": "/static/media/itm.8901800c.png"
+  },
+  {
+    "revision": "48187bdc53744cfd394b42874daf50e3",
+    "url": "/static/media/kakobrands.48187bdc.png"
+  },
+  {
+    "revision": "5e9cdd950d53539a03d228892fc8875a",
+    "url": "/static/media/building.5e9cdd95.jpg"
+  },
+  {
+    "revision": "c7a05349604fc322c93146f452033b53",
+    "url": "/index.html"
+  }
+];
