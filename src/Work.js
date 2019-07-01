f2bd99b402ef2bef5b941b62073f11fb3b67d25f
Landry Kapela <landrykapela@gmail.com>
2019-07-02 01:05:30 +0300

    Add neelansoft to work list

--- src/Work.js
+++ src/Work.js
@@ -1,44 +1,89 @@
-import React from 'react';
-import fk from './assets/fk_logo.png';
-import abc from './assets/abc.jpg';
-import ubj from './assets/ubj.png';
-import jmx from './assets/jamirex.jpg';
-import itm from './assets/itm.png';
-import kakobrands from './assets/kakobrands.png';
+import React from "react";
+import fk from "./assets/fk_logo.png";
+import abc from "./assets/abc.jpg";
+import ubj from "./assets/ubj.png";
+import jmx from "./assets/jamirex.jpg";
+import itm from "./assets/itm.png";
+import kakobrands from "./assets/kakobrands.png";
+import neelansoft from "./assets/neelansoft.png";
 
-class Work extends React.Component{
-
-  constructor(props){
+class Work extends React.Component {
+  constructor(props) {
     super(props);
     this.getClients = this.getClients.bind(this);
     this.populate = this.populate.bind(this);
-    this.state = {clients: [{id:0,name:'FK Law Chambers',link:'http://fklawchambers.co.tz',image:fk},{id:1,name:'ABC Dental Clinic',link:'http://abcdentalclinic.co.tz',image:abc},{id:2,name:'UBJ Law Chamber', link:'http://ubjlawchamber.co.tz',image:ubj},{id:3,name:'Jamirex Hotel',link:'http://jamirexhotel.co.tz',image:jmx},{id:4,name:'ITM Tanzania Ltd',link:'http://registration.itmafrica.co.tz',image:itm},{id:5,name:'Kakobrands',link:'https://kakobrands.com',image:kakobrands}]};
+    this.state = {
+      clients: [
+        {
+          id: 0,
+          name: "FK Law Chambers",
+          link: "https://fklawchambers.co.tz",
+          image: fk
+        },
+        {
+          id: 1,
+          name: "ABC Dental Clinic",
+          link: "https://abcdentalclinic.co.tz",
+          image: abc
+        },
+        {
+          id: 2,
+          name: "UBJ Law Chamber",
+          link: "http://ubjlawchamber.co.tz",
+          image: ubj
+        },
+        {
+          id: 3,
+          name: "Jamirex Hotel",
+          link: "https://jamirexhotel.co.tz",
+          image: jmx
+        },
+        {
+          id: 4,
+          name: "ITM Tanzania Ltd",
+          link: "http://registration.itmafrica.co.tz",
+          image: itm
+        },
+        {
+          id: 5,
+          name: "Kakobrands",
+          link: "https://kakobrands.com",
+          image: kakobrands
+        },
+        {
+          id: 6,
+          name: "Neelansoft Technologies",
+          link: "https://neelansoft.co.tz",
+          image: neelansoft
+        }
+      ]
+    };
   }
-  setClients(clients){
-    this.setState = {clients:clients};
+  setClients(clients) {
+    this.setState({ clients: clients });
   }
-  getClients(){
+  getClients() {
     return this.state.clients;
   }
-  componentDidMount(){
+  componentDidMount() {
     this.populate();
   }
-  populate(){
+  populate() {
     return (
       <div className="work-content" id="work-content">
-
         <h1 id="work-content-title">Projects I've Worked On</h1>
         <div className="work-details">
-      {
-        this.getClients().map((data)=>(
-
-            <a className="work-item" key={data['id']} href={data['link']}>
-              <img src={data['image']} alt={data['name']}/>
+          {this.getClients().map(data => (
+            <a className="work-item" key={data["id"]} href={data["link"]}>
+              <img src={data["image"]} alt={data["name"]} />
             </a>
-      ))}</div></div>
+          ))}
+        </div>
+      </div>
     );
   }
-  render(){
+  render() {
     return this.populate();
   }
-} export default Work;
+}
+export default Work;
